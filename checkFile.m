83c95c9f9e3cd9514a310d04333fe9adb3ddcc28
Edie1995 <42354484+Edie1995@users.noreply.github.com>
2019-04-16 00:17:22 +0300

    dodanie możliwości wpisywania skrótów aminokwasów

--- checkFile.m
+++ checkFile.m
@@ -4,7 +4,7 @@
 %funkcja nic nie zwraca.
 %permission - lista dopuszczalnych znaków w sekwencji
 function [] = checkFile(sequence)
-permission = '[A,C,G,T,U,"",newline]';
+permission = '[A,C,D,E,F,G,H,I,K,L,M,N,P,Q,R,S,T,V,W,Y,U,"",newline]';
 for i = 1:length(sequence)
    if(~regexp(permission,sequence(i)))
       error("zly kod");
