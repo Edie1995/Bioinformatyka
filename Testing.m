e0a3544ccd2c47cdb067d9c6de414382f6efdfc0
Edie1995 <42354484+Edie1995@users.noreply.github.com>
2019-05-24 00:56:18 +0300

    funkcja z przykładowymi testami

---
+++ Testing.m
@@ -0,0 +1,12 @@
+
+
+% Wywyolanie funkcji z sekwencjami odczytanymi ze strony NCBI
+main(1,-2,1,'.','z2Stron','KC591872.1','KC985188.1','JQ735459.1');
+ 
+% %Wywyo�anie funkcji z sekwencjami odczytanymi z plików
+main(1,-2,1,'.','z2Stron','seq1.fasta','seq2.fasta','seq3.fasta','seq4.fasta');
+
+
+% %Wywyo�anie funkcji z pierwszych 2 sekwencji ze strony NCBI a drugich dwóch z
+% %pliku
+ main(1,-2,1,'.','z2Stron','KC591872.1','seq1.fasta','seq2.fasta')
